7e4f4c8bd70462b7d98c8eb334cb2d7744e7b405
socialchorus <developers@socialchorus.com>
2014-06-18 18:33:12 -0700

    uprserter

--- lib/repossessed/upserter.rb
+++ lib/repossessed/upserter.rb
@@ -1,4 +1,45 @@
 module Repossessed
   class Upserter
+    attr_accessor :persistence_class, :attrs
+    attr_writer :find_keys
+
+    def initialize(opts={})
+      @persistence_class = opts[:persistence_class]
+      @attrs = opts[:attrs]
+    end
+
+    def save
+      if record
+        record.update_attributes(save_attrs)
+      else
+        create
+      end
+
+      record
+    end
+
+    def create
+      @record = persistence_class.create(save_attrs)
+    end
+
+    def save_attrs
+      attrs.slice(*save_keys)
+    end
+
+    def record
+      @record ||= persistence_class.where(find_attrs).take
+    end
+
+    def find_attrs
+      attrs.slice(*find_keys)
+    end
+
+    def find_keys
+      @find_attrs ||= [:id]
+    end
+
+    def save_keys
+      attrs.keys - find_keys
+    end
   end
 end
--- spec/upserter_spec.rb
+++ spec/upserter_spec.rb
@@ -1,6 +1,69 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/mini_test'
 
 describe Repossessed::Upserter do
+  let(:persistence_class) {
+    stub('AP persistence class', {
+      where: stub(take: record),
+      create: record
+    })
+  }
+
+  let(:attrs) {
+    {
+      id: 323,
+      name: 'name'
+    }
+  }
+
+  let(:record) {
+    stub('AR record', {
+      update_attributes: true
+    })
+  }
+
+  describe "when all the needed stuff is passed in" do
+    let(:upserter) {
+      Repossessed::Upserter.new({
+        persistence_class: persistence_class,
+        attrs: attrs
+      })
+    }
+
+    describe 'when the record is new' do
+      let(:record) { nil }
+      let(:new_record) { mock('new record') }
+
+      before do
+        persistence_class.stubs(:create).returns(new_record)
+      end
+
+      it 'creates the record' do
+        persistence_class.expects(:create).with(name: attrs[:name]).returns(new_record)
+        upserter.save
+      end
+
+      it 'returns the record' do
+        upserter.save.must_equal new_record
+      end
+    end
+
+    describe 'when the record already can be found by id' do
+      it 'finds the record in question' do
+        persistence_class.expects(:where).with(id: attrs[:id]).returns(stub(take: record))
+        upserter.save
+      end
+
+      it 'updates the record' do
+        record.expects(:update_attributes).with(name: attrs[:name])
+        upserter.save
+      end
+
+      it 'returns the record' do
+        upserter.save.must_equal record
+      end
+    end
+  end
 end
