882b92113ba7f8c3e5aa93fe4c390c2e5d5b6232
socialchorus <developers@socialchorus.com>
2014-06-26 20:34:28 -0700

    builder method for constructing a class

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -1,4 +1,43 @@
 require 'spec_helper'
 
 describe Repossessed::Builder do
+  let(:repo_class) {
+    Repossessed::Builder.config(persistence_class) do |config|
+      config.allowed_keys = [:name, :email, :dob]
+    end
+  }
+
+  let(:params) {
+    {
+      name: 'Kane Baccigalupi',
+      email: 'kane@socialchorus.com',
+      password: 'secret',
+      password_confirmation: 'secret',
+      dob: 'once upon a time'
+    }
+  }
+
+  let(:persistence_class) {
+    double('AR persistence class', {
+      where: double(take: found_record),
+      create: found_record
+    })
+  }
+
+  let(:found_record) {
+    nil
+  }
+
+  it "builds a viable coordinator class" do
+    repo_class.new(params).perform.should == {
+      json: {
+        name: params[:name],
+        email: params[:email],
+        dob: params[:dob],
+        errors: {}
+      },
+
+      status: 200
+    }
+  end
 end
